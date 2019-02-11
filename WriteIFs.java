19c15d7f92cb2dca5d48ac98b692da9ca2b55924
Kate Bayard <katherinebayard@zipcoders-MBP-6.lan>
2019-02-10 21:38:05 -0500

    completed IFs

--- WriteIFs.java
+++ WriteIFs.java
@@ -11,7 +11,11 @@ public class WriteIFs
     public void playerDied(boolean player1) {
         // Write an IF statement that checks “player1.isAlive()” 
         // and if that’s false, calls “displayGameOver(player1)”
-     
+        
+        if (!isAlive(player1)) {
+            displayGameOver(player1);
+        }
+        
     }
     
     public String thermoSTAT(int room) {
@@ -19,7 +23,9 @@ public class WriteIFs
         // “temperature(room)” and if that check is less than 70, 
         // calls “heatOn()” else calls “coolOn()”
 
-
+        if (room < 70) {
+            heatOn();
+        } else {coolOn();}
         
         return this.ss;
     }
@@ -31,14 +37,19 @@ public class WriteIFs
         // “insideTemp()” is less than 62, 
         // calls “startAFire(fireplace1)”
 
+        if ((outsideTemp() < 50) && (insideTemp() < 62)){
+            startAFire(fireplace1);
     }
+}
 
     public void checkFuel(double fuelLevel) {
         // Write an IF statement that checks “fuelLevel” 
         // and if that check is less than 0.08, calls “refuel()”
 
+        if (fuelLevel <0.08) {
+            refuel();
     }
-
+}
 
     
     /**
